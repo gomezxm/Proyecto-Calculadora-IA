6fa82c58df903cd8e4ab55d52657fc5a35233549
DanielUbarte <daniel.ubarte@utp.ac.pa>
2025-10-25 21:06:54 -0500

    creación de form

--- ProyectCalculadora/Form1.Designer.cs
+++ ProyectCalculadora/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(336, 450);
+            ClientSize = new Size(342, 450);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
