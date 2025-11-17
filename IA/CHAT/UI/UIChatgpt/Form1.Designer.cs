c7eed3bdd50cc28f5b0e20027092a2d4a127a289
gomezxm <miguel.gomez1@utp.ac.pa>
2025-11-16 23:01:38 -0500

    He realizado las bases del UI del chatgtp

---
+++ IA/CHAT/UI/UIChatgpt/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace UIChatgpt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPrompt = new System.Windows.Forms.TextBox();
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(690, 553);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 71);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPrompt
+            // 
+            this.txtPrompt.Location = new System.Drawing.Point(66, 531);
+            this.txtPrompt.Multiline = true;
+            this.txtPrompt.Name = "txtPrompt";
+            this.txtPrompt.Size = new System.Drawing.Size(580, 116);
+            this.txtPrompt.TabIndex = 1;
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new System.Drawing.Point(66, 12);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(580, 486);
+            this.txtChat.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(792, 694);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.txtPrompt);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ChatGPT – Cliente WinForms";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPrompt;
+        private System.Windows.Forms.TextBox txtChat;
+    }
+}
+
